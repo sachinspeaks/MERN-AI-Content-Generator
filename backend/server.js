59d9565808abf2b975b98caf7ac2e98009d55bea
Sachin <sachingaud78@gmail.com>
2023-12-30 21:46:47 +0530

    cors5

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,44 @@ require('dotenv').config();
 const express = require('express')
 const cron = require('node-cron')
 const app = express()
+require('./utils/connectDB')()
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const usersRouter = require('./routes/usersRouter')
 const openAIRouter = require('./routes/openAIRouter')
 const stripeRouter = require('./routes/stripeRouter')
-require('./utils/connectDB')()
 const { errorHandler } = require('./middlewares/errorMiddleware');
 const User = require('./models/User');
 
 
 const PORT = process.env.PORT || 5000
 
-const corsOptions = {
-    origin: "https://mern-ai-content-generator-mium.vercel.app",
-    credentials: true,
-}
-app.use(cors(corsOptions))
+// const corsOptions = {
+//     origin: ["https://mern-ai-content-generator-mium.vercel.app"],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//     credentials: true,
+// }
+// app.use(cors(corsOptions))
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://mern-ai-content-generator-mium.vercel.app"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+
+    next();
+});
 
 //for trial period : runs every single day
 cron.schedule("0 0 * * * *", async () => {
